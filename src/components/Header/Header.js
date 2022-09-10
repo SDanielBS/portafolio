19d96a905166f20f8c949627fbdf1c25e07ba303
Sergio Bernuy <bernuysergio.12@gmail.com>
2022-09-09 23:58:49 -0500

    setting project to start coding

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <header className="header">
+      <nav>
+        <ul>
+          <li>Nav</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export { Header }
---
+++ src/pages/Main.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Header } from "../components/Header/Header";
+
+const Main = () => {
+  return (
+    <React.Fragment>
+      <Header/>
+      <main>
+        Main
+      </main>
+    </React.Fragment>
+  );
+}
+
+export { Main }
