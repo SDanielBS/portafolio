8e1ec4482ec7275fb9e598e03180e057d3660102
Sergio Bernuy <bernuysergio.12@gmail.com>
2022-10-21 20:24:32 -0500

    change render mode to react 18

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import { Portafolio } from './pages/Portafolio/Portafolio';
 
 import './index.scss';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// REACTJS 18
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <Portafolio/>
+//   </React.StrictMode>
+// );
+
+// REACTJS 18+
+const root = createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <Portafolio/>
-  </React.StrictMode>
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
