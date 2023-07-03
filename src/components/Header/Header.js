ecede50acd72072955b8b8a80f8689e43aa27532
Sergio Bernuy <bernuysergio.12@gmail.com>
2023-07-03 00:46:16 -0500

    Set responsive layouts

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,24 @@
 // import { BrowserRouter } from "react-router-dom";
+import { useRef, useState } from "react";
 import { Logo } from "../Logo/Logo";
 import { MainNavigation } from "../MainNavigation/MainNavigation";
 import "./Header.scss"
 
 const Header = () => {
+  let [isChecked, setCheck] = useState(false)
+  const toggleMenu = () => {
+    setCheck(!isChecked)
+  }
   return (
     <header className="header">
       <div className="header-container">
         <Logo/>
-        <MainNavigation/>
+        <MainNavigation
+          shouldShow={isChecked}
+        />
+        <button className="hamburguer" onClick={toggleMenu}>
+          <span className="hamburguer_line" />
+        </button>
       </div>
     </header>
   );
--- src/components/MainNavigation/MainNavigation.js
+++ src/components/MainNavigation/MainNavigation.js
@@ -1,8 +1,24 @@
+import { useEffect, useRef } from "react"
 import "./MainNavigation.scss"
 
-const MainNavigation = () => {
+const MainNavigation = (props) => {
+  let mainNavigationRef = useRef()
+  const shouldShowMenu = () => {
+    if (props.shouldShow) {
+      mainNavigationRef.current.classList.add("showMenu");
+      mainNavigationRef.current.classList.remove("hideMenu");
+    } else if (mainNavigationRef.current.classList.contains("showMenu")) {
+      mainNavigationRef.current.classList.add("hideMenu");
+      mainNavigationRef.current.classList.remove("showMenu");
+    }
+  }
+
+  useEffect(() => {
+    shouldShowMenu()
+  },[props.shouldShow]);
+
   return(
-    <nav className="mainNavigation">
+    <nav className="mainNavigation" ref={mainNavigationRef}>
       <ul className="mainNavigation-list">
         <li className="mainNavigation-list-item"><a href="#">Proyectos</a></li>
         <li className="mainNavigation-list-item"><a href="#">Skills</a></li>
